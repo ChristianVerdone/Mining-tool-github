fd792b0fa5eda7ad50465f5a5c8353cda62ccc93
Marco Zamparelli <mzamparelli@microgame.it>
2023-11-13 18:05:16 +0100

    import and save issue in file.json with timestamp

--- import_and_save_issue.py
+++ import_and_save_issue.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 def print_and_save_github_issues():
     # Richiedi all'utente di inserire l'owner e il repository
@@ -16,6 +17,14 @@ def print_and_save_github_issues():
         # La risposta è avvenuta con successo
         issues = response.json()
 
+        # Aggiungi un timestamp alle informazioni delle issue
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        for issue in issues:
+            issue['timestamp'] = timestamp
+
+        # Costruisci il nome del file JSON con il timestamp nel titolo
+        file_name = f'issues_{timestamp}.json'
+
         # Stampa le informazioni sulle issue sulla console
         for issue in issues:
             print(f"Issue #{issue['number']}:")
@@ -23,13 +32,14 @@ def print_and_save_github_issues():
             print(f"  Stato: {issue['state']}")
             print(f"  Autore: {issue['user']['login']}")
             print(f"  URL: {issue['html_url']}")
+            print(f"  Timestamp: {issue['timestamp']}")
             print('\n' + '-'*30 + '\n')  # Separatore per chiarezza
 
         # Salva le informazioni delle issue in un file JSON
-        with open('issues.json', 'w', encoding='utf-8') as json_file:
+        with open(file_name, 'w', encoding='utf-8') as json_file:
             json.dump(issues, json_file, ensure_ascii=False, indent=4)
 
-        print(f"Le informazioni delle issue sono state salvate con successo nel file 'issues.json'")
+        print(f"Le informazioni delle issue sono state salvate con successo nel file '{file_name}'")
     else:
         print(f"Errore nella richiesta: {response.status_code}")
 
