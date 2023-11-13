16ce1ba40f25f975e4f7fef9d2c928afc62d2f4a
Christian Verdone <christian_ve98@hotmail.it>
2023-11-13 11:15:39 +0100

    Esecuzione continua del tool

--- mainTool.py
+++ mainTool.py
@@ -1,51 +1,40 @@
 import requests
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description='Un esempio di tool a riga di comando.')
 
-    parser.add_argument('nome', nargs='?', default=None, help='Il nome per il messaggio di saluto.')
-    parser.add_argument('--azione', choices=['saluta', 'calcola'], help='Azione da eseguire.')
+    parser.add_argument('AccessToken', nargs='?', default=None, help='Il token di accesso per API token')
+    parser.add_argument('--azione', choices=['importIssue', 'ImportCommits'], help='Azione da eseguire.')
 
-    args = parser.parse_args()
+    while True:
+        args = parser.parse_args()
 
-    if args.nome is None:
-        args.nome = input("Inserisci il tuo nome: ")
+        if args.nome is None:
+            args.nome = input("Inserisci il tuo Token di accesso API GitHub: ")
 
-    if args.azione == 'saluta':
-        print(f'Ciao, {args.nome}! Benvenuto nel mio tool a riga di comando.')
-    elif args.azione == 'calcola':
-        # Esempio di un'azione di calcolo (puoi personalizzarla in base alle tue esigenze)
-        risultato = len(args.nome)
-        print(f'Il risultato del calcolo sulla lunghezza del nome è: {risultato}')
-    else:
-        print(f'Azione non riconosciuta. Le opzioni valide sono: saluta, calcola.')
-
-    # Replace 'owner' and 'repo_name' with the actual owner and repository name
-    owner = 'owner'
-    repo_name = 'repository_name'
-
-    repo_info = get_github_repo(owner, repo_name)
-
-    if repo_info:
-        # Print the retrieved repository information
-        print(f"Repository Name: {repo_info['name']}")
-        print(f"Owner: {repo_info['owner']['login']}")
-        print(f"Description: {repo_info['description']}")
-        print(f"URL: {repo_info['html_url']}")
-        # Add more fields as needed
-    else:
-        print("Unable to fetch repository information.")
+        if args.azione == 'importIssue':
+            print(f'Ciao, {args.nome}! Benvenuto nel mio tool a riga di comando.')
+        elif args.azione == 'calcola':
+            risultato = len(args.nome)
+            print(f'Il risultato del calcolo sulla lunghezza del nome è: {risultato}')
+        elif args.azione == 'esci':
+            print('Arrivederci!')
+            break  # Esci dal loop
+        else:
+            print(f'Azione non riconosciuta. Le opzioni valide sono: saluta, calcola, esci.')
 
 if __name__ == '__main__':
     main()
+
 
 def get_github_repo(owner, repo_name):
     base_url = 'https://api.github.com/repos'
     api_url = f'{base_url}/{owner}/{repo_name}'
 
     # Provide your GitHub API token if you have one
-    headers = {'Authorization': 'Bearer YOUR_GITHUB_TOKEN'}  # Replace with your GitHub token
+    headers = {'Authorization': 'Bearer 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'}  # Replace with your GitHub token
 
     # Make the GET request to the GitHub API
     response = requests.get(api_url, headers=headers)
