3160a0f7159e8c7a66c7168f14baee7182d6b19e
Marco Zamparelli <mzamparelli@microgame.it>
2023-11-16 22:44:36 +0100

    called the workflow logs class in main and made some changes to test the workflow logs class

--- import_and_save_workflow_logs.py
+++ import_and_save_workflow_logs.py
@@ -1,63 +1,75 @@
 import os
 import requests
-import os
 import json
 from datetime import datetime
+import request_error_handler
 
-def print_and_save_workflow_logs():
-    # Richiedi all'utente di inserire l'owner e il repository
-    owner = input("Inserisci il nome dell'owner (utente su GitHub): ")
-    repository = input("Inserisci il nome del repository su GitHub: ")
+def request_github_workflows(token, owner, repository):
+    api_url = f'https://api.github.com/repos/{owner}/{repository}/actions/workflows'
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(api_url, headers=headers)
+    return response
 
-    # Costruisci l'URL dell'API GitHub per ottenere i workflow logs
-    api_url = f'https://api.github.com/repos/{owner}/{repository}/actions/runs'
+def request_github_workflow_runs(token, owner, repository, workflow_id):
+    api_url = f'https://api.github.com/repos/{owner}/{repository}/actions/workflows/{workflow_id}/runs'
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(api_url, headers=headers)
+    return response
 
-    # Esegui la richiesta GET all'API di GitHub Actions
-    response = requests.get(api_url)
+def get_and_save_all_github_workflow_logs(token, owner, repository):
+    workflows_response = request_github_workflows(token, owner, repository)
 
-    if response.status_code == 200:
-        # La risposta è avvenuta con successo
-        workflow_runs = response.json()
+    if workflows_response.status_code == 200:
+        workflows = workflows_response.json()
 
-        # Crea una directory per i log dei flussi di lavoro
-        workflow_dir = os.path.join(repository, "workflow_logs")
-        os.makedirs(workflow_dir, exist_ok=True)
+        for workflow in workflows['workflows']:
+            workflow_id = workflow['id']
+            workflow_name = workflow['name']
 
-        # Aggiungi un timestamp alle informazioni dei workflow logs
-        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            print(f"Ottenimento dei logs per il workflow: {workflow_name}")
 
-        # Creare la cartella principale con il nome del repository
-        repository_folder = f'{repository}_data'
-        if not os.path.exists(repository_folder):
-            os.makedirs(repository_folder)
+            response = request_github_workflow_runs(token, owner, repository, workflow_id)
 
-        # Creare la sottocartella con il nome "workflow_logs"
-        workflow_logs_folder = os.path.join(repository_folder, 'workflow_logs')
-        if not os.path.exists(workflow_logs_folder):
-            os.makedirs(workflow_logs_folder)
-
-        # Costruisci il percorso del file JSON con il timestamp nel titolo
-        file_path = os.path.join(workflow_logs_folder, f'workflow_logs_{timestamp}.json')
-
-        # Stampa le informazioni sui workflow logs sulla console
-        for run in workflow_runs['workflow_runs']:
-            print(f"Workflow Run #{run['id']}:")
-            print(f"  Nome: {run['name']}")
-            print(f"  Stato: {run['status']}")
-            print(f"  Concluso: {run['conclusion']}")
-            print(f"  URL: {run['html_url']}")
-            print(f"  Timestamp: {timestamp}")
-            print('\n' + '-'*30 + '\n')  # Separatore per chiarezza
-
-        # Salva le informazioni dei workflow logs in un file JSON
-        # Salva i dati localmente (esempio: in un file JSON)
-        log_file_path = os.path.join(workflow_dir, file_path)
-        with open(log_file_path, 'w', encoding='utf-8') as json_file:
-            json.dump(workflow_runs, json_file, ensure_ascii=False, indent=4)
-
-        print(f"I logs dei workflow sono stati salvati con successo nel file '{file_path}'")
+            if response.status_code == 200:
+                workflow_runs = response.json()
+                save_workflow_logs(repository, workflow_name, workflow_runs)
+                print_workflow_info(workflow_runs)
+            else:
+                request_error_handler(response.status_code)
     else:
-        print(f"Errore nella richiesta: {response.status_code}")
+        request_error_handler(response.status_code)
 
-# Chiamare la funzione per ottenere, stampare e salvare i logs dei workflow di un repository specifico
-print_and_save_workflow_logs()
+def save_workflow_logs(repository, workflow_name, workflow_runs):
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    workflow_logs_folder = make_workflow_logs_directory(repository)
+    file_path = os.path.join(workflow_logs_folder, f'{workflow_name}_logs_{timestamp}.json')
+
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(workflow_runs, json_file, ensure_ascii=False, indent=4)
+
+    print(f"I logs del workflow '{workflow_name}' sono stati salvati con successo nel file '{file_path}'")
+
+def make_workflow_logs_directory(repository):
+    repository_folder = f'{repository}_data'
+    if not os.path.exists(repository_folder):
+        os.makedirs(repository_folder)
+
+    workflow_logs_folder = os.path.join(repository_folder, 'workflow_logs')
+    if not os.path.exists(workflow_logs_folder):
+        os.makedirs(workflow_logs_folder)
+
+    return workflow_logs_folder
+
+def print_workflow_info(workflow_runs):
+    for run in workflow_runs['workflow_runs']:
+        print(f"\nInformazioni per il run {run['run_number']}:")
+        print(f"  Stato: {run['status']}")
+        print(f"  Concluso: {run['conclusion']}")
+        print(f"  Creato il: {run['created_at']}")
+        print(f"  Durata: {run['duration'] // 60} min {run['duration'] % 60} sec")
+
+# Esegui la funzione per ottenere e salvare i workflow logs
+token = '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'  # Sostituisci con il tuo token GitHub
+owner = input("Inserisci il nome dell'owner (utente su GitHub): ")
+repository = input("Inserisci il nome del repository su GitHub: ")
+get_and_save_all_github_workflow_logs(token, owner, repository)
