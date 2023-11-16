4b53c3444658c8321bad3111e5129a80464af9cb
gerardocentrella <centrella.gerardo0@gmail.com>
2023-11-16 19:06:00 +0100

    added new status code error

--- request_error_handler.py
+++ request_error_handler.py
@@ -11,6 +11,22 @@ def request_error_handler(status_code):
     elif(status_code == 403):
         print(f"Request error: {status_code}")
         print("403 FORBIDDEN: The request has been refused. See the accompanying message for the specific reason (most likely for exceeding rate limit).")
-   
+        
+    elif(status_code == 406):
+        print(f"Request error: {status_code}")
+        print("NOT ACCEPTABLE: The request specified an invalid format.")
+    
+    elif(status_code == 500):
+        print(f"Request error: {status_code}")
+        print("INTERNAL SERVER ERROR: Something is horribly wrong.")
+        
+    elif(status_code == 503):
+        print(f"Request error: {status_code}")
+        print("SERVICE UNAVAILABLE: The service is up, but overloaded with requests. Try again later.")
+    
+    elif(status_code == 504): 
+        print(f"Request error: {status_code}")
+        print("GATEWAY TIMEOUT: Servers are up, but the request couldn’t be serviced due to some failure within our stack. Try again later.")
+    
     else:
        print("Generic request error")
