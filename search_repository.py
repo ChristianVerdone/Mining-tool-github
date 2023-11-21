cbb5a8f7a1ff6740d63fd5f47b58caa18b61ae3b
Christian Verdone <christian_ve98@hotmail.it>
2023-11-21 21:05:46 +0100

    Some refactor fix, full mining issues

--- search_repository.py
+++ search_repository.py
@@ -2,13 +2,14 @@ import os
 import requests
 import request_error_handler
 
+
 def controller_repo(token):
-    path=input("Indica il path del file: ")
-    path_file=f_path_txt(path)
-    
-    #Leggi da file .txt l'owner/repository
+    path = input("Indica il path del file: ")
+    path_file = f_path_txt(path)
+
+    # Leggi da file .txt l'owner/repository
     with open(path_file, 'r') as file:
-        #Per ogni riga del file prendi l'owner e il repository 
+        # Per ogni riga del file prendi l'owner e il repository
         lines = file.readlines()
         for line in lines:
             owner, repository = line.strip().split('/')  # Assume che il formato sia 'owner/repository'
@@ -27,28 +28,27 @@ def controller_repo(token):
                 request_error_handler.request_error_handler(response.status_code)
             '''
 
+
 def request_github(token, owner, repository):
-     # Costruisci l'URL dell'API GitHub
+    # Costruisci l'URL dell'API GitHub
     api_url = f'https://api.github.com/repos/{owner}/{repository}'
-     # Utilizza il token di Github per autenticarsi 
+    # Utilizza il token di Github per autenticarsi
     headers = {'Authorization': 'Bearer ' + token}
-     # GET request al GitHub API
+    # GET request al GitHub API
     response = requests.get(api_url, headers=headers)
 
     return response
+
 
 def f_path_txt(path):
     if not os.path.exists(path):
         print(f"Il percorso '{path}' non esiste ")
         return
-        
-    file=input("Inserisci il nome del file txt: ")
-    path_file=f"{path}/{file}.txt"
+
+    file = input("Inserisci il nome del file txt: ")
+    path_file = f"{path}/{file}.txt"
 
     if not os.path.exists(path_file):
         print(f"Il percorso '{path_file}' non esiste.")
         return
-    return path_file       
-
-
-   
+    return path_file
