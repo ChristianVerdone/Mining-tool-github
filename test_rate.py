a31591079acf5298a522a4cae67a7e9830148446
Angelo-Conte <76561590+Angelo-Conte@users.noreply.github.com>
2023-12-07 10:39:54 +0100

    Added test: test_rate_limit_handler.py and modified the test: test_rate.py

--- test_rate.py
+++ test_rate.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import time
 import pytest
 from rate_limit import rate_minute
+from unittest.mock import patch
 
 # Test della funzione rate_minute
 def test_rate_minute_below_limit():
@@ -20,25 +21,23 @@ def test_rate_minute_above_limit():
     mainTool.start_time = time.time() - 30         # Imposta il tempo in modo che sia trascorso meno di un minuto
     mainTool.requests_count = 950                  # Numero di richieste superiore al limite
     
-    rate_minute()
+    with patch('time.sleep') as mock_sleep:
+        rate_minute()
 
     # Verifica che il conteggio delle richieste e il tempo di inizio siano stati resettati dopo il superamento del limite
     assert mainTool.requests_count == 0
     assert mainTool.start_time == time.time()
+    mock_sleep.assert_called()  # Verifica che la funzione time.sleep è stata chiamata 
 
 def test_rate_minute_reset():
     
-    mainTool.start_time = time.time() - 85       # Imposta il tempo in modo che sia trascorso meno di un minuto
+    mainTool.start_time = time.time() - 85       # Imposta il tempo in modo che sia trascorso più di un minuto
     mainTool.requests_count = 800                # Numero di richieste inferiori al limite
     print(f"Pre-Reset: {mainTool.requests_count} e {mainTool.start_time}")
     
     rate_minute()
    
-    assert mainTool.requests_count == 0          # Verifica che il conteggio delle richieste e il tempo di inizio non siano stati modificati
+    assert mainTool.requests_count == 0          # Verifica che il conteggio delle richieste e il tempo di inizio siano stati modificati
     assert mainTool.start_time == mainTool.start_time
     print(f"Post-Reset: {mainTool.requests_count} e {mainTool.start_time}")
 
-
-#test_rate_minute_below_limit()
-#test_rate_minute_above_limit()
-#test_rate_minute_reset()
